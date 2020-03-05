27a8ed5563d543fe4be8172f461c39841a8e619d
Jonathan Mainguy <jon@soh.re>
2020-03-05 15:12:49 -0500

    drop -v from tar, so it doesnt clutter docker log

--- build/run.sh
+++ build/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tar zxvf /opt/k8sCapcity_Linux_x86_64.tar.gz -C /opt/
+tar zxf /opt/k8sCapcity_Linux_x86_64.tar.gz -C /opt/
 if [ -z "$NODELABEL" ]; then
     /opt/k8sCapcity --daemon
 else
