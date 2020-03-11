f483c829a20e53e20e4e39e327636e54571f8d19
Jonathan Mainguy <jon@soh.re>
2020-03-11 12:20:49 -0400

    daemon now 100 percent test coverage

---
+++ cmd/k8sCapcity/daemon_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"k8s.io/apimachinery/pkg/api/resource"
+	"testing"
+)
+
+func TestDaemonModeEmpty(t *testing.T) {
+	clusterInfo := ClusterInfo{}
+	runDaemonMode(clusterInfo)
+}
+
+func TestDaemonModeValidClusterInfo(t *testing.T) {
+	clusterInfo := ClusterInfo{
+		ClusterAllocatableMemory:         resource.MustParse("1Gi"),
+		ClusterAllocatableCPU:            resource.MustParse("1"),
+		ClusterAllocatablePods:           resource.MustParse("1000"),
+		RqclusterAllocatedLimitsMemory:   resource.MustParse("1Gi"),
+		RqclusterAllocatedLimitsCPU:      resource.MustParse("1"),
+		RqclusterAllocatedPods:           resource.MustParse("100"),
+		RqclusterAllocatedRequestsMemory: resource.MustParse("1Gi"),
+		RqclusterAllocatedRequestsCPU:    resource.MustParse("1"),
+		NminusCPU:                        resource.MustParse("1"),
+		NminusMemory:                     resource.MustParse("1Gi"),
+		NminusPods:                       resource.MustParse("10"),
+		NodeLabel:                        "test=true",
+		NodeInfo: map[string]NodeInfo{
+			"test-node": {
+				AllocatableCPU:     resource.MustParse("16"),
+				AllocatableMemory:  resource.MustParse("256Gi"),
+				AllocatablePods:    resource.MustParse("1000"),
+				UsedPods:           10,
+				UsedCPU:            resource.MustParse("1"),
+				UsedMemory:         resource.MustParse("1Gi"),
+				UsedMemoryRequests: resource.MustParse("10Gi"),
+				UsedMemoryLimits:   resource.MustParse("10Gi"),
+				UsedCPURequests:    resource.MustParse("1"),
+				PrintOutput:        true,
+			},
+		},
+	}
+	runDaemonMode(clusterInfo)
+}
