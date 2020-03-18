480f4fce9b8d5460c31bd5d62b5fbfc456d45b2b
Jonathan Mainguy <jon@soh.re>
2020-03-18 12:58:29 -0400

    update cicd

--- deployments/kubernetes/docker/deploy.sh
+++ deployments/kubernetes/docker/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=v0.2.1
+VERSION=v0.2.3
 docker build -t=push.soh.re/k8scapcity:$VERSION .
 docker tag push.soh.re/k8scapcity:$VERSION push.soh.re/k8scapcity:latest
 docker push push.soh.re/k8scapcity:$VERSION
