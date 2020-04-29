5cbdf2c0df9b0492cc65a44b981fb1e3f4b04f1e
Jonathan Seth Mainguy <jon@soh.re>
2020-04-29 13:39:49 -0400

    support gcp and other auth

--- cmd/k8sCapcity/main.go
+++ cmd/k8sCapcity/main.go
@@ -13,6 +13,8 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"time"
+	// Support gcp and other authentication schemes
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func check(err error) {
